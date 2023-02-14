e3aae1148bfd4fdc50bf868357915bf3373c04bd
Lyndon Panton <LyndonPanton@gmail.com>
2023-02-14 01:07:14 +0000

    Bullets are now destroyed after 2 seconds

--- Asteroids/Assets/Scripts/Bullet.cs
+++ Asteroids/Assets/Scripts/Bullet.cs
@@ -13,9 +13,36 @@ public class Bullet : MonoBehaviour
     /// </summary>
     Rigidbody2D rb2d;
 
+    /// <summary>
+    /// The amount of time before the game object is automatically destroyed
+    /// </summary>
+    const float totalLifeTime = 2.0f;
+
+    /// <summary>
+    /// Stores the amount of time the game object has been alive
+    /// </summary>
+    Timer deathTimer;
+
     void Awake()
     {
         rb2d = GetComponent<Rigidbody2D>();
+    }
+    void Start()
+    {
+        deathTimer = GetComponent<Timer>();
+        deathTimer.Duration = totalLifeTime;
+        deathTimer.Run();
+    }
+
+    /// <summary>
+    /// Update is called once per frame
+    /// </summary>
+    void Update()
+    {
+        if (deathTimer.Finished)
+        {
+            Destroy(gameObject);
+        }
     }
 
     /// <summary>
